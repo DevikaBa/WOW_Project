d9e2fbe841f249fd1b9cb3380367f903b9ecca81
gary brown <garybrown@garys-mbp-2.attlocal.net>
2021-04-19 21:46:29 -0700

    updateZipcode

--- flask/app.py
+++ flask/app.py
@@ -38,23 +38,28 @@ def search():
 
     elif request.method == 'POST':
         df = pd.read_csv("DoctorData.csv", index_col=0)
-        user_input = request.form.get('user_input')
-        # user_zip = request.form.get('user_zip')
+        df['Zipcode'] = df['Zipcode'].astype(int).astype(str)
+        # user_input = request.form.get('user_input')
+        user_zip = request.form.get('user_zip')
          # specialty
-        df = df[df.Specialty == user_input]
+        # df = df[df.Specialty == user_input]
+        df = df[df.Zipcode == user_zip]
+
         
         
         flash("congrats, you have found your doctor") 
 
 
 
-        print(user_input)
+        
    
         return render_template("patient_list.html", column_names=df.columns.values, row_data=list(df.values.tolist()),
                            link_column="Patient ID", zip=zip)
     
+
         
         
+      
 
         
        
